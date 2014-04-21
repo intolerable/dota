6ba1f003f09a116e147248823d2b6a74b46ee344
Fraser Murray <fraser.m.murray@gmail.com>
2014-04-21 21:21:59 +0100

    fixed the enums for game modes

--- Dota/WebAPI/Types/GameModes.hs
+++ Dota/WebAPI/Types/GameModes.hs
@@ -16,4 +16,42 @@ data GameMode = NoMode
               | NewPlayerPool
               | CompendiumMatchmaking
               | CaptainsDraft
-  deriving (Show, Read, Eq, Enum)
+              | UnknownMode
+  deriving (Show, Read, Eq)
+
+instance Enum GameMode where
+  fromEnum UnknownMode = -1
+  fromEnum NoMode = 0
+  fromEnum AllPick = 1
+  fromEnum CaptainsMode = 2
+  fromEnum RandomDraft = 3
+  fromEnum SingleDraft = 4
+  fromEnum AllRandom = 5
+  fromEnum Intro = 6
+  fromEnum Diretide = 7
+  fromEnum ReverseCaptainsMode = 8
+  fromEnum Greeviling = 9
+  fromEnum Tutorial = 10
+  fromEnum MidOnly = 11
+  fromEnum LeastPlayed = 12
+  fromEnum NewPlayerPool = 13
+  fromEnum CompendiumMatchmaking = 14
+  fromEnum CaptainsDraft = 16
+
+  toEnum 0 = NoMode
+  toEnum 1 = AllPick
+  toEnum 2 = CaptainsMode
+  toEnum 3 = RandomDraft
+  toEnum 4 = SingleDraft
+  toEnum 5 = AllRandom
+  toEnum 6 = Intro
+  toEnum 7 = Diretide
+  toEnum 8 = ReverseCaptainsMode
+  toEnum 9 = Greeviling
+  toEnum 10 = Tutorial
+  toEnum 11 = MidOnly
+  toEnum 12 = LeastPlayed
+  toEnum 13 = NewPlayerPool
+  toEnum 14 = CompendiumMatchmaking
+  toEnum 16 = CaptainsDraft
+  toEnum _ = UnknownMode
