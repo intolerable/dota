c5acb0d05d41b9b21d1be3093baec941564aa87f
Fraser Murray <fraser.m.murray@gmail.com>
2014-04-21 15:53:11 +0100

    removed dist/

--- dist/build/autogen/Paths_dota.hs
+++ dist/build/autogen/Paths_dota.hs
@@ -17,11 +17,11 @@ version :: Version
 version = Version {versionBranch = [0,1,0,0], versionTags = []}
 bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/Users/Fraser/.cabal/bin"
-libdir     = "/Users/Fraser/.cabal/lib/x86_64-osx-ghc-7.8.2/dota-0.1.0.0"
-datadir    = "/Users/Fraser/.cabal/share/x86_64-osx-ghc-7.8.2/dota-0.1.0.0"
-libexecdir = "/Users/Fraser/.cabal/libexec"
-sysconfdir = "/Users/Fraser/.cabal/etc"
+bindir     = "/Users/Fraser/Code/dota/.cabal-sandbox/bin"
+libdir     = "/Users/Fraser/Code/dota/.cabal-sandbox/lib/x86_64-osx-ghc-7.8.2/dota-0.1.0.0"
+datadir    = "/Users/Fraser/Code/dota/.cabal-sandbox/share/x86_64-osx-ghc-7.8.2/dota-0.1.0.0"
+libexecdir = "/Users/Fraser/Code/dota/.cabal-sandbox/libexec"
+sysconfdir = "/Users/Fraser/Code/dota/.cabal-sandbox/etc"
 
 getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "dota_bindir") (\_ -> return bindir)
