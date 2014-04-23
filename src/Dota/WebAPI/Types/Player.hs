2a4dc3b5f46b1b25dc64c4506e811040f16935fc
Fraser Murray <fraser.m.murray@gmail.com>
2014-04-23 20:03:22 +0100

    added player_slot support

--- src/Dota/WebAPI/Types/Player.hs
+++ src/Dota/WebAPI/Types/Player.hs
@@ -7,11 +7,14 @@ import Control.Monad (liftM)
 import Data.Monoid
 import Data.Aeson
 import Data.Aeson.Types (Parser)
+import Data.Bits
+import Data.Word (Word8)
 
 newtype AccountID = AccountID Integer
   deriving (Show, Eq)
 
 data Player = Player { accountID :: Maybe AccountID
+                     , playerSlot :: PlayerSlot
                      , hero :: Hero
                      , kda :: (Integer, Integer, Integer)
                      , level :: Integer
@@ -29,6 +32,7 @@ instance FromJSON Player where
                     <*> o .: "deaths"
                     <*> o .: "assists"
     Player <$> liftM (fmap AccountID) (o .:? "account_id")
+           <*> o .: "player_slot"
            <*> (toEnum <$> o .: "hero_id")
            <*> kda'
            <*> o .: "level"
@@ -59,4 +63,19 @@ instance FromJSON BasicPlayer where
     BasicPlayer <$> liftM (fmap AccountID) (o .:? "account_id")
                 <*> o .: "player_slot"
                 <*> (toEnum <$> o .: "hero_id")
+  parseJSON _ = mempty
+
+data PlayerSlot = PlayerSlot { playerTeam :: Team
+                             , teamPosition :: Int}
+  deriving (Show, Eq)
+
+instance FromJSON PlayerSlot where
+  -- player slot is a byte where
+  --   T.......: T is whether the team is Radiant
+  --   .....SSS: S is the slot (0-4) of the player
+  parseJSON (Number n) = do
+    let bits = floor n :: Word8
+    let team = if bits `testBit` 7 then Radiant else Dire
+    let position = bits .&. 7
+    pure (PlayerSlot team (fromIntegral position))
   parseJSON _ = mempty
