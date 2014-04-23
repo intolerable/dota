ab52b6b9c647bed866657c0c2df47282ca3df353
Fraser Murray <fraser.m.murray@gmail.com>
2014-04-23 20:42:39 +0100

    tests on player

--- test/Tests/WebAPI/Types/Player.hs
+++ test/Tests/WebAPI/Types/Player.hs
@@ -3,6 +3,7 @@ module Tests.WebAPI.Types.Player (tests) where
 import Test.HUnit
 
 import Dota.WebAPI.Types.Player
+import Dota.WebAPI.Types.Basics
 
 import APIBuilder
 import qualified Data.ByteString.Lazy as BS (readFile) 
@@ -14,8 +15,22 @@ tests = "Player" ~:
 
 testPlayer :: Test
 testPlayer = "testPlayer" ~: do
-  _ <- player
-  False @? "not implemented yet"
+  p <- player
+  case p of
+    Right x -> checkExamplePlayer x
+    Left _ -> assertFailure "should decode properly"
+
+checkExamplePlayer :: Player -> Assertion
+checkExamplePlayer p = do
+  cs p @?= (157,2)
+  kda p @?= (1,4,5)
+  playerSlot p @?= PlayerSlot Radiant 0
+  items p @?= ( Dust
+              , Armlet
+              , BlinkDagger
+              , StoutShield
+              , Desolator
+              , PowerTreads )
 
 player :: IO (Either (APIError ()) Player)
 player = do
