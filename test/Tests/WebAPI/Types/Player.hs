c45b891db8e03cb1880916a6127af003c198c189
Fraser Murray <fraser.m.murray@gmail.com>
2014-04-23 20:26:49 +0100

    added test outline for Player, w/ an example

--- test/Tests/WebAPI/Types/Player.hs
+++ test/Tests/WebAPI/Types/Player.hs
@@ -4,5 +4,20 @@ import Test.HUnit
 
 import Dota.WebAPI.Types.Player
 
+import APIBuilder
+import qualified Data.ByteString.Lazy as BS (readFile) 
+import System.Directory (canonicalizePath)
+
 tests :: Test
-tests = "Player" ~: TestList []
+tests = "Player" ~:
+  test [ testPlayer ]
+
+testPlayer :: Test
+testPlayer = "testPlayer" ~: do
+  _ <- player
+  False @? "not implemented yet"
+
+player :: IO (Either (APIError ()) Player)
+player = do
+  file <- canonicalizePath "test/json/player.json" >>= BS.readFile
+  return $ decode file
